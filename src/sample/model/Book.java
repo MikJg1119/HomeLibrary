35019ef1175804eea15056fc0843d280eebef4a9
Mikołaj J <mikigraf@wp.pl>
2021-07-04 16:37:19 +0200

    update of algorithm, that downloads book details, it now uses jsoup library and gets the required details, downloading algorithm moved do Book constructor

--- src/sample/model/Book.java
+++ src/sample/model/Book.java
@@ -17,33 +17,32 @@ public class Book {
     private String title;
     private Author author;
     private String isbn;
+    private StringBuilder urlBuild = new StringBuilder("https://isbnsearch.org/isbn/");
 
     public Book(String isbn) {
         this.isbn = isbn;
-
-    }
-
-    private StringBuilder urlBuild = new StringBuilder("https://isbnsearch.org/isbn/");
-
-    public Book getBookByISBN (String isbn) throws IOException {
         urlBuild.append(isbn);
-        URL url = new URL(urlBuild.toString());
-        URLConnection conn = url.openConnection();
-        Document webSite = Jsoup.connect(url.toString()).userAgent("mozilla/17.0").get();
+        try{
 
-        Elements results = webSite.select("h2.title");
-        for (Element result:results){
-            System.out.println(result.getElementsByTag("span").first().text());
-            String title = result.getElementsByTag("span").first().text();
+            Document webSite = Jsoup.connect(urlBuild.toString()).userAgent("mozilla/17.0").get();
+
+            Elements results = webSite.select("h2.title");
+            for (Element result : results) {
+                System.out.println(result.getElementsByTag("span").first().text());
+                this.title = result.getElementsByTag("span").first().text();
+            }
+            results = webSite.select("p.author");
+            for (Element result : results) {
+                System.out.println(result.getElementsByTag("strong").first().text());
+                this.author = new Author(result.getElementsByTag("strong").first().text());
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
         }
-        results = webSite.select("p.author");
-        for (Element result:results){
-            System.out.println(result.getElementsByTag("strong").first().text());
-            String authorName = result.getElementsByTag("strong").first().text();
-        }
-
-        return new Book(isbn);
-
-
     }
+
+
+
+
 }
