104fd7777c83194adcc8efaf7892391c8b5e07b4
Mikołaj J <mikigraf@wp.pl>
2021-07-04 16:19:55 +0200

    update of algorithm, that downloads book details, it now uses jsoup library and gets the required details

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module HomeLibrary {
     requires javafx.fxml;
     requires java.xml;
     requires java.sql;
+    requires org.jsoup;
     opens sample;
     opens sample.model;
 }
--- src/sample/model/Book.java
+++ src/sample/model/Book.java
@@ -1,4 +1,49 @@
 package sample.model;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Book {
+    private String title;
+    private Author author;
+    private String isbn;
+
+    public Book(String isbn) {
+        this.isbn = isbn;
+
+    }
+
+    private StringBuilder urlBuild = new StringBuilder("https://isbnsearch.org/isbn/");
+
+    public Book getBookByISBN (String isbn) throws IOException {
+        urlBuild.append(isbn);
+        URL url = new URL(urlBuild.toString());
+        URLConnection conn = url.openConnection();
+        Document webSite = Jsoup.connect(url.toString()).userAgent("mozilla/17.0").get();
+
+        Elements results = webSite.select("h2.title");
+        for (Element result:results){
+            System.out.println(result.getElementsByTag("span").first().text());
+            String title = result.getElementsByTag("span").first().text();
+        }
+        results = webSite.select("p.author");
+        for (Element result:results){
+            System.out.println(result.getElementsByTag("strong").first().text());
+            String authorName = result.getElementsByTag("strong").first().text();
+        }
+
+        return new Book(isbn);
+
+
+    }
 }
