47c23ed227b317f93c7677cfc8485165c26533c2
Mikołaj J <mikigraf@wp.pl>
2021-07-05 14:34:47 +0200

    update of details downloaded for the book

--- src/sample/model/Book.java
+++ src/sample/model/Book.java
@@ -17,26 +17,41 @@ public class Book {
     private String title;
     private Author author;
     private String isbn;
-    private StringBuilder urlBuild = new StringBuilder("https://isbnsearch.org/isbn/");
+    private StringBuilder urlBuild = new StringBuilder("https://www.abebooks.com/servlet/SearchResults?sts=t&isbn=");
+    private String publisher;
+    private String publishedYear;
+    private String language=null;
 
     public Book(String isbn) {
         this.isbn = isbn;
         urlBuild.append(isbn);
         try{
 
-            Document webSite = Jsoup.connect(urlBuild.toString()).userAgent("mozilla/17.0").get();
-
+            Document webSite = Jsoup.connect(urlBuild.toString()).userAgent("mozilla/17.0").get(); //connection to the website
+            // getting title based on ISBN
             Elements results = webSite.select("h2.title");
             for (Element result : results) {
-                System.out.println(result.getElementsByTag("span").first().text());
                 this.title = result.getElementsByTag("span").first().text();
             }
+            // getting author
             results = webSite.select("p.author");
             for (Element result : results) {
-                System.out.println(result.getElementsByTag("strong").first().text());
                 this.author = new Author(result.getElementsByTag("strong").first().text());
             }
+            // getting publisher
+            results = webSite.select("span.opt-publisher");
+            for (Element result:results){
+                this.publisher = result.getElementsByTag("span").first().text();
+            }
+            //getting year published
+            results = webSite.select("span.opt-publish-date");
+            for (Element result:results){
+                this.publishedYear =  result.getElementsByTag("span").first().text();
+            }
 
+            if (this.language==null){
+                this.language="Polski";
+            }
         }catch (IOException e){
             e.printStackTrace();
         }
